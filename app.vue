f9bffa3aea6964fb75747bee5c7d51146ddca784
devdouglassilva <devdouglassilva@gmail.com>
2024-03-09 18:40:56 +0000

    Remove(code unnecessary): remove Loadder trigger unnecessary how don't are using async

--- app.vue
+++ app.vue
@@ -1,18 +1,8 @@
 <template>
   <div>
     <NuxtLoadingIndicator />
-    <Loadder v-if="!pageLoaded" />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
-<script setup lang="ts">
-const pageLoaded = ref(false);
-useNuxtApp().hook('page:start', () => {
-  pageLoaded.value = false
-})
-useNuxtApp().hook('page:finish', () => {
-  pageLoaded.value = true
-})
-</script>
