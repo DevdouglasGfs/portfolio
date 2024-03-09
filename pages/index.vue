e882e0bb167cbfdadbef05155628d0ccadd37c58
devdouglassilva <devdouglassilva@gmail.com>
2024-03-09 18:27:22 +0000

    Update(hero page): Add basic seo settings

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 definePageMeta({
     name: "home",
+});
+useSeoMeta({
+    title: "Douglas Silva - Portfólio",
+    ogTitle: "Douglas Silva - Portfólio",
+    description: "Portfólio do programador Douglas Silva",
+    ogDescription: "Portfólio de Douglas Silva",
+    ogLocale: "pt-br",
+    ogType: "profile",
+    twitterCard: "summary"
 })
 
 const pageLoaded = ref(false);
@@ -13,8 +22,9 @@ useNuxtApp().hook('page:finish', () => {
 </script>
 <template>
     <Loadder v-if="!pageLoaded" />
-    <main class="grid grid-cols-2 auto-rows-fr gap-56 lg:grid-cols-4 *:col-span-2 *:lg:col-span-4">
+    <main class="grid grid-cols-2 auto-rows-min gap-56 lg:grid-cols-4 *:col-span-2 *:lg:col-span-4">
         <Hero />
         <WhoIAm />
+        <Skills />
     </main>
 </template>
