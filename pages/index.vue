001348291cb27499e4322f9e58a1cb2652f5f618
devdouglassilva <devdouglassilva@gmail.com>
2024-03-09 14:02:31 +0000

    Add(Hero page): Add the index page

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+definePageMeta({
+    name: "home",
+})
+
+const pageLoaded = ref(false);
+useNuxtApp().hook('page:start', () => {
+    pageLoaded.value = false
+})
+useNuxtApp().hook('page:finish', () => {
+    pageLoaded.value = true
+})
+</script>
+<template>
+    <Loadder v-if="!pageLoaded" />
+    <main class="grid grid-cols-2 auto-rows-fr gap-56 lg:grid-cols-4 *:col-span-2 *:lg:col-span-4">
+        <Hero />
+        <WhoIAm />
+    </main>
+</template>
