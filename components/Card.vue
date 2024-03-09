786a7eb08997aef00dca05d098044eb40fb9646e
devdouglassilva <devdouglassilva@gmail.com>
2024-03-09 14:00:40 +0000

    Add(card): Component for describe something

---
+++ components/Card.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+    title: string;
+    icon: string;
+}>();
+</script>
+<template>
+    <button
+        class="p-3 lg:p-6 rounded-sd ring-1 bg-ntl-800 ring-ntl-600 flex flex-col gap-2 transition max-lg:duration-200 duration-500 ease-in hover:active:ring-ntl-400 hover:active:bg-ntl-700 text-left">
+        <Icon :name="icon" class="text-lg text-secundary-400" />
+        {{ title }}
+    </button>
+</template>
