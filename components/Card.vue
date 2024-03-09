30af04e9c652beba8658cff3f544c0420324c52e
devdouglassilva <devdouglassilva@gmail.com>
2024-03-09 18:21:05 +0000

    Feat(iconLg): New prop to define if show icon lg

--- components/Card.vue
+++ components/Card.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-defineProps<{
+export type cardProps = {
     title: string;
-    icon: string;
-}>();
+    icon?: string;
+    iconLg?: boolean;
+}
+defineProps<cardProps>();
 </script>
 <template>
     <button
-        class="p-3 lg:p-6 rounded-sd ring-1 bg-ntl-800 ring-ntl-600 flex flex-col gap-2 transition max-lg:duration-200 duration-500 ease-in hover:active:ring-ntl-400 hover:active:bg-ntl-700 text-left">
-        <Icon :name="icon" class="text-lg text-secundary-400" />
+        class="p-3 lg:p-6 capitalize rounded-sd ring-1 bg-ntl-800 ring-ntl-600 flex flex-col gap-2 transition max-lg:duration-200 duration-500 ease-in hover:active:ring-ntl-400 hover:active:bg-ntl-700 text-left">
+        <Icon v-if="icon && iconLg" :name="icon" class="text-secundary-400" :class="['text-3xl']" />
+        <Icon v-else-if="icon" :name="icon" class="text-lg text-secundary-400" />
         {{ title }}
     </button>
 </template>
