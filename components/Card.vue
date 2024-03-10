69c1075cfd89e00dd3acfcafd38a8b2d6fc33e85
devdouglassilva <devdouglassilva@gmail.com>
2024-03-10 20:40:02 +0000

    Change(Card): Add more styling Add style for hover and focus state

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 export type cardProps = {
-    title: string;
+    title?: string;
     icon?: string;
     iconLg?: boolean;
 }
@@ -8,9 +8,9 @@ defineProps<cardProps>();
 </script>
 <template>
     <button
-        class="p-3 lg:p-6 capitalize rounded-sd ring-1 bg-ntl-800 ring-ntl-600 flex flex-col gap-2 transition max-lg:duration-200 duration-500 ease-in hover:active:ring-ntl-400 hover:active:bg-ntl-700 text-left">
-        <Icon v-if="icon && iconLg" :name="icon" class="text-secundary-400" :class="['text-3xl']" />
-        <Icon v-else-if="icon" :name="icon" class="text-lg text-secundary-400" />
+        class="p-3 lg:p-6 capitalize rounded-sd ring-1 bg-ntl-800 ring-ntl-600 flex flex-col gap-2 transition max-lg:duration-200 duration-500 ease-in hover:active:ring-ntl-400 hover:active:bg-ntl-700 text-left group">
+        <Icon v-if="icon && iconLg" :name="icon" class="text-secundary-400 text-3xl transition-colors duration-200 group-hover:text-secundary-300" />
+        <Icon v-else-if="icon" :name="icon" class="text-lg text-secundary-400 transition-colors duration-200 group-hover:text-secundary-300" />
         {{ title }}
     </button>
 </template>
