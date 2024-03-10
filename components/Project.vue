1f1aafc3edbd9142d6ec3d44baa54b73b9ef14e7
devdouglassilva <devdouglassilva@gmail.com>
2024-03-10 20:53:41 +0000

    Update(Project): Add more features Add info about the status of project; Remove event listener @keyup.space; Change the appearance

--- components/Project.vue
+++ components/Project.vue
@@ -11,7 +11,7 @@ const toggleDetails = useToggle(showTechnicalDetails);
         <div class="flex flex-col self-stretch gap-3 md:landscape:col-span-1">
             <div class="flex flex-wrap items-center gap-1 justify-between self-stretch">
                 <h3 class="text-lg lg:text-2xl capitalize font-bold leading-normal">{{ title }}</h3>
-                <div class="text-md lg:text-lg leading-normal"><time :datetime="started">
+                <div class="text-md lg:text-lg font-ternary leading-normal"><time :datetime="started">
                         {{ typeof started === 'string' ? started : useDateFormat(started,
                             'DD/MM/YYYY').value.toString() }}</time>
                     —
@@ -31,40 +31,48 @@ const toggleDetails = useToggle(showTechnicalDetails);
                 </div>
                 <NuxtLink :to="deployUrl"
                     class="px-6 max-md:landscape:py-2 py-3 rounded-tl-xl bg-secundary-400 transition-colors max-lg:duration-200 duration-300 ease-in hover:active:bg-secundary-500 absolute z-20 bottom-0 right-0 text-ntl-900"
-                    :class="[{ '!bg-ntl-700 !text-ntl-0': !deployUrl && image }, { '!bg-ntl-900 !text-ntl-0': !deployUrl && !image }]">
+                    :class="[{ '!bg-ntl-700 !text-ntl-100': !deployUrl && image }, { '!bg-ntl-900 !text-ntl-100': !deployUrl && !image }]">
                     <Icon class="text-2xl text-current" name="material-symbols:preview" />
                     <span class="sr-only">Ir para o deploy do projeto {{ title }}</span>
                 </NuxtLink>
             </div>
         </div>
         <div class="flex flex-col self-stretch gap-3 md:landscape:col-span-1">
-            <h3 class="text-lg lg:text-2xl font-bold leading-normal">Detalhes</h3>
-            <div class="flex flex-col gap-8 w-full">
-                <div class="flex flex-col gap-3">
+            <h4 class="text-lg lg:text-2xl font-bold leading-normal">Detalhes</h4>
+            <div class="w-full">
+                <div class="flex self-stretch flex-col gap-3">
                     <div tabindex="0" class="flex self-stretch items-center gap-3 py-1" @click="toggleDetails()"
-                        @keyup.space.exact="toggleDetails()" @keyup.enter.exact="toggleDetails()">
-                        <h4 class="text-md lg:text-lg mt-2 capitalize leading-normal">Tecnólogias e
-                            frameworks</h4>
+                        @keyup.enter.exact="toggleDetails()">
+                        <h5 class="text-md lg:text-lg mt-2 capitalize leading-normal">Tecnólogias e
+                            frameworks</h5>
                         <Icon class="text-lg text-ntl-100" v-if="showTechnicalDetails" name="mdi:arrow-collapse-vertical" />
                         <Icon class="text-lg text-ntl-100" v-else name="mdi:arrow-expand-vertical" />
                     </div>
-                    <ul class="flex flex-wrap gap-3">
-                        <li v-show="showTechnicalDetails" class="flex-auto basis-3/12" v-for="skill in tecnologies">
-                            <Card class="w-full p-6 rounded-xl" :title="skill.title" :icon="skill.icon" />
+                    <ul v-show="showTechnicalDetails"
+                        class="self-stretch flex flex-wrap items-center gap-3 w-full max-h-60 overflow-y-scroll snap-y snap-mandatory p-1 hidden-scrollbar overscroll-none rounded-xl ring-1 ring-ntl-700">
+                        <li class="flex-initial basis-3/12 snap-always snap-center w-full" v-for="skill in tecnologies">
+                            <Card tabindex="-1" class="w-full items-center p-6 rounded-xl" :title="skill.title"
+                                :icon="skill.icon" />
                         </li>
                     </ul>
                 </div>
-                <div class="flex flex-col self-stretch gap-1">
-                    <h3 class="text-md lg:text-lg capitalize leading-normal">Descrição</h3>
+                <div class="flex flex-col self-stretch gap-1 mt-1">
+                    <p class="text-md lg:text-lg capitalize">Status:
+                        <span class="text-primary-600 first-letter:capitalize normal-case">
+                            {{ status === 'done' ? 'Finalizado'
+                                : status === 'in progress' ? 'Em progresso'
+                                    : 'Pausado' }}
+                        </span>
+                    </p>
+                    <h5 class="text-md lg:text-lg capitalize leading-normal">Descrição</h5>
                     <div class="flex flex-col gap-2">
                         <p class="text-base lg:text-md tracking-wide leading-relaxed">{{ description }}</p>
-                        <NuxtLink
-                            class="inline-flex item-center justify-center text-base lg:text-md font-bold leading-tight px-6 pt-3 pb-1 rounded-sd button bg-gradient-to-b from-primary-500 to-primary-700 hover:active:from-primary-600 hover:active:to-primary-700 tracking-wide"
-                            :class="{ 'bg-ntl-700 font-normal': !github }" :to="github">
+                        <NuxtLink v-if="github" :to="github" target="_blank"
+                            class="inline-flex item-center justify-center text-base lg:text-md font-bold leading-tight px-6 pt-3 pb-1 rounded-sd button bg-gradient-to-b from-primary-500 to-primary-700 hover:active:from-primary-600 hover:active:to-primary-700 tracking-wide focus:outline-secundary-200 focus:outline">
                             Visitar o Github do projeto</NuxtLink>
                     </div>
                 </div>
             </div>
-    </div>
-</article>
+        </div>
+    </article>
 </template>
