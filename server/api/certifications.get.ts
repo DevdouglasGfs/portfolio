34ca5141fd4d38671e8a82fec677fd58729490a4
devdouglassilva <devdouglassilva@gmail.com>
2024-04-26 20:39:43 +0000

    Feat: Consume projects and certifications via API Also: - Add new project WhyApp;

---
+++ server/api/certifications.get.ts
@@ -0,0 +1,12 @@
+import { CertificationDTO } from "~/components/Certifications.vue";
+
+const certificationsList: Array<CertificationDTO> = [{
+    title: "Técnico de informática",
+    started: "18/02/2021",
+    finished: "18/12/2023",
+    description: "Ao logo deste curso tive contato com diversos tópicos e conteúdos, entre eles estão: Lógica de Programação; Design; Redes de Computadores; HTML; CSS; JavaScript; PHP; SQL; Java; Banco de Dados; Pensamento computacional; Robótica; entre outros temas."
+}];
+
+export default defineEventHandler((event) => {
+    return certificationsList
+})
