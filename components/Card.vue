705136ea56779369e0032a32a249cfd1b67724a9
devdouglassilva <devdouglassilva@gmail.com>
2024-04-27 08:56:38 +0000

    Styles: align text and icon correctly

--- components/Card.vue
+++ components/Card.vue
@@ -18,6 +18,6 @@ defineProps<CardProps>();
         class="p-3 capitalize rounded-sd ring-1 bg-ntl-800 ring-ntl-600 flex items-center gap-2 transition max-lg:duration-200 duration-500 ease-in hover:active:ring-ntl-400 hover:active:bg-ntl-700 text-left group">
         <Icon v-if="icon && iconLg" :name="icon" class="text-secundary-400 text-3xl transition-colors duration-200 group-hover:text-secundary-300" />
         <Icon v-else-if="icon" :name="icon" class="text-lg text-secundary-400 transition-colors duration-200 group-hover:text-secundary-300" />
-        {{ title }}
+        <span class="-mb-1">{{ title }}</span>
     </button>
 </template>
