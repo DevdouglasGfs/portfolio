669f4f7bc7eefd43b1222f4e87356752b2f13ba1
devdouglassilva <devdouglassilva@gmail.com>
2024-04-27 09:08:32 +0000

    Style: Increase text leading(line height)

--- components/Hero.vue
+++ components/Hero.vue
@@ -77,7 +77,7 @@ const topics: Array<CardProps & { to: string }> = [{
                                 Silva</span>
                         </div>
                     </hgroup>
-                    <p class="text-base tracking-wide max-w-prose leading-tight lg:text-md">Olá, seja bem-vindo(a) ao
+                    <p class="text-base tracking-wide max-w-prose leading-loose lg:text-md">Olá, seja bem-vindo(a) ao
                         meu portfólio.<br /><br />
                         Separei alguns tópicos para você conhecer a mim, o meu trabalho e minhas qualificações para
                         estar atuando na área e na sua empresa</p>
