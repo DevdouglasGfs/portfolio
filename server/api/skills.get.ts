d70f7b83316ecead980f37751ecedc7b06108eca
devdouglassilva <devdouglassilva@gmail.com>
2024-04-27 09:11:34 +0000

    Chore: Add react to hardSkills

--- server/api/skills.get.ts
+++ server/api/skills.get.ts
@@ -1,72 +1,98 @@
 import { CardProps } from "~/components/Card.vue";
 
-const softSkills: Array<CardProps> = [{
+const softSkills: Array<CardProps> = [
+  {
     title: "Autodidata",
-    icon: "uil:book-reader"
-}, {
+    icon: "uil:book-reader",
+  },
+  {
     title: "Resiliente",
-    icon: "mdi:timer-alert-outline"
-}, {
+    icon: "mdi:timer-alert-outline",
+  },
+  {
     title: "Adaptativo",
-    icon: "uil:exclamation-circle"
-}, {
+    icon: "uil:exclamation-circle",
+  },
+  {
     title: "Determinado",
-    icon: "mdi:package-variant-closed"
-}, {
+    icon: "mdi:package-variant-closed",
+  },
+  {
     title: "Adepto a opiniões",
-    icon: "mdi:smiley-wink-outline"
-}, {
+    icon: "mdi:smiley-wink-outline",
+  },
+  {
     title: "Criativo",
-    icon: "mdi:palette-outline"
-}, {
+    icon: "mdi:palette-outline",
+  },
+  {
     title: "Proativo",
-    icon: "mdi:hand-wave-outline"
-}, {
+    icon: "mdi:hand-wave-outline",
+  },
+  {
     title: "Trabalho em equipe",
-    icon: "fluent:people-team-16-regular"
-}, {
+    icon: "fluent:people-team-16-regular",
+  },
+  {
     title: "Perseverante",
-    icon: "tabler:user-code"
-}]
-const hardSkills: Array<CardProps> = [{
+    icon: "tabler:user-code",
+  },
+];
+const hardSkills: Array<CardProps> = [
+  {
     title: "Vue",
-    icon: "uil:vuejs"
-}, {
+    icon: "uil:vuejs",
+  },
+  {
+    title: "React",
+    icon: "uil:react",
+  },
+  {
     title: "Nuxt",
-    icon: "mdi:nuxt"
-}, {
+    icon: "mdi:nuxt",
+  },
+  {
     title: "Vite",
-    icon: "file-icons:vite"
-}, {
+    icon: "file-icons:vite",
+  },
+  {
     title: "Vitest",
-    icon: "simple-icons:vitest"
-}, {
+    icon: "simple-icons:vitest",
+  },
+  {
     title: "JavaScript",
-    icon: "mdi:language-javascript"
-}, {
+    icon: "mdi:language-javascript",
+  },
+  {
     title: "TypeScript",
-    icon: "mdi:language-typescript"
-}, {
+    icon: "mdi:language-typescript",
+  },
+  {
     title: "Figma",
-    icon: "fa-brands:figma"
-},
-{
+    icon: "fa-brands:figma",
+  },
+  {
     title: "Sass",
-    icon: "mdi:sass"
-}, {
+    icon: "mdi:sass",
+  },
+  {
     title: "Git",
-    icon: "mdi:git"
-}, {
+    icon: "mdi:git",
+  },
+  {
     title: "CSS",
-    icon: "mdi:language-css3"
-}, {
+    icon: "mdi:language-css3",
+  },
+  {
     title: "HTML",
-    icon: "mdi:language-html5"
-}, {
+    icon: "mdi:language-html5",
+  },
+  {
     title: "Tailwind",
-    icon: "mdi:tailwind"
-}]
+    icon: "mdi:tailwind",
+  },
+];
 
 export default defineEventHandler((event) => {
-    return {soft: softSkills, hard: hardSkills}
-})
+  return { soft: softSkills, hard: hardSkills };
+});
